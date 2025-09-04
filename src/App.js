4f44d174303079c2a436ef24589e02a070c64448
Jaspreet Singh <166392990+quriousCoder3953@users.noreply.github.com>
2025-09-04 16:31:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,61 +1,127 @@
 import {Component} from 'react'
+
 import {Switch, Route, Redirect} from 'react-router-dom'
-import AppContext from './Context/AppContext'
+
+import LoginRoute from './components/Login'
+import HomeRoute from './components/Home'
 import ProtectedRoute from './components/ProtectedRoute'
-import Login from './components/Login'
-import Home from './components/Home'
-import Trending from './components/Trending'
-import Gaming from './components/Gaming'
-import VideoDetails from './components/VideoDetails'
-import SavedVideos from './components/SavedVideos'
-import NotFound from './components/NotFound'
+import TrendingRoute from './components/Trending'
+import GamingRoute from './components/Gaming'
+import VideoItemDetailsRoute from './components/VideoItemDetails'
+import SavedVideosRoute from './components/SavedVideosRoute'
+import NotFoundRoute from './components/NotFound'
+
+import NxtWatchAppContext from './Context/NxtWatchAppContext'
 
 import './App.css'
 
 class App extends Component {
-  state = {savedList: [], lightTheme: true}
+  state = {
+    isDarkModeEnabled: false,
+    savedVideosList: [],
+    videosReactionList: [],
+  }
 
-  changeTheme = () => {
+  onToggleDarkMode = () => {
     this.setState(prevState => ({
-      lightTheme: !prevState.lightTheme,
+      isDarkModeEnabled: !prevState.isDarkModeEnabled,
     }))
   }
 
-  updateSavedList = video => {
-    this.setState(prevState => ({
-      savedList: [...prevState.savedList, video],
-    }))
+  onSaveVideo = videoDetails => {
+    const {savedVideosList} = this.state
+    const updatedVideoDetails = {...videoDetails, isVideoSaved: true}
+    this.setState({
+      savedVideosList: [...savedVideosList, updatedVideoDetails],
+    })
   }
 
-  removeVideo = video => {
-    this.setState(prevState => ({
-      savedList: prevState.savedList.filter(each => each.id !== video.id),
-    }))
+  onUnSaveVideo = id => {
+    const {savedVideosList} = this.state
+    const filteredList = savedVideosList.filter(eachitem => eachitem.id !== id)
+    this.setState({savedVideosList: filteredList})
+  }
+
+  onLikeVideo = id => {
+    const {videosReactionList} = this.state
+    const filteredList = videosReactionList.filter(
+      eachitem => eachitem.id === id,
+    )
+    if (filteredList.length === 0) {
+      this.setState({
+        videosReactionList: [
+          ...videosReactionList,
+          {id, isVideoLiked: true, isVideoDisLiked: false},
+        ],
+      })
+    } else {
+      const newList = videosReactionList.map(eachitem => {
+        if (eachitem.id === id) {
+          return {id, isVideoLiked: true, isVideoDisLiked: false}
+        }
+        return eachitem
+      })
+      this.setState({videosReactionList: newList})
+    }
+  }
+
+  onDislikeVideo = id => {
+    const {videosReactionList} = this.state
+    const filteredList = videosReactionList.filter(
+      eachitem => eachitem.id === id,
+    )
+    if (filteredList.length === 0) {
+      this.setState({
+        videosReactionList: [
+          ...videosReactionList,
+          {id, isVideoLiked: false, isVideoDisLiked: true},
+        ],
+      })
+    } else {
+      const newList = videosReactionList.map(eachitem => {
+        if (eachitem.id === id) {
+          return {id, isVideoLiked: false, isVideoDisLiked: true}
+        }
+        return eachitem
+      })
+      this.setState({videosReactionList: newList})
+    }
   }
 
   render() {
-    const {savedList, lightTheme} = this.state
+    const {isDarkModeEnabled, savedVideosList, videosReactionList} = this.state
     return (
-      <AppContext.Provider
+      <NxtWatchAppContext.Provider
         value={{
-          savedList,
-          lightTheme,
-          changeTheme: this.changeTheme,
-          updateSavedList: this.updateSavedList,
-          removeVideo: this.removeVideo,
+          isDarkModeEnabled,
+          savedVideosList,
+          videosReactionList,
+          onToggleDarkMode: this.onToggleDarkMode,
+          onSaveVideo: this.onSaveVideo,
+          onUnSaveVideo: this.onUnSaveVideo,
+          onLikeVideo: this.onLikeVideo,
+          onDislikeVideo: this.onDislikeVideo,
         }}
       >
         <Switch>
-          <Route exact path="/login" component={Login} />
-          <ProtectedRoute exact path="/" component={Home} />
-          <ProtectedRoute exact path="/trending" component={Trending} />
-          <ProtectedRoute exact path="/gaming" component={Gaming} />
-          <ProtectedRoute exact path="/videos/:id" component={VideoDetails} />
-          <ProtectedRoute exact path="/saved-videos" component={SavedVideos} />
-          <Route path="/not-found" component={NotFound} />
-          <Redirect to="not-found" />
+          <Route exact path="/login" component={LoginRoute} />
+          <ProtectedRoute exact path="/" component={HomeRoute} />
+          <ProtectedRoute exact path="/trending" component={TrendingRoute} />
+          <ProtectedRoute exact path="/gaming" component={GamingRoute} />
+          <ProtectedRoute
+            exact
+            path="/videos/:id"
+            component={VideoItemDetailsRoute}
+          />
+          <ProtectedRoute
+            exact
+            path="/saved-videos"
+            component={SavedVideosRoute}
+          />
+          <ProtectedRoute exact path="/not-found" component={NotFoundRoute} />
+          <Redirect to="/not-found" />
         </Switch>
-      </AppContext.Provider>
+      </NxtWatchAppContext.Provider>
     )
   }
 }
